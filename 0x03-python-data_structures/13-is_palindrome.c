1ce62b23d48b0135f523ba4dd4175d93ccbce30a
wiza2rd <hadosh22@hotmail.com>
2023-12-04 11:58:07 -0800

    update

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,72 +1,41 @@
 #include "lists.h"
-
-listint_t *reverse_listint(listint_t **head);
-int is_palindrome(listint_t **head);
-
 /**
- * reverse_listint - Reverses a singly-linked listint_t list.
- * @head: A pointer to the starting node of the list to reverse.
+ * palindrome - check if is palindrome with recursion
+ * @l: l
+ * @r: r
  *
- * Return: A pointer to the head of the reversed list.
+ * Return: 1 palindrome, 0 not palindrome
  */
-listint_t *reverse_listint(listint_t **head)
+int palindrome(listint_t **l, listint_t *r)
 {
-	listint_t *node = *head, *next, *prev = NULL;
+	int response;
 
-	while (node)
+	if (r != NULL)
 	{
-		next = node->next;
-		node->next = prev;
-		prev = node;
-		node = next;
-	}
+		response = palindrome(l, r->next);
+		if (response != 0)
+		{
+			response = (r->n == (*l)->n);
+			*l = (*l)->next;
+			return (response);
+		}
+		return (0);
 
-	*head = prev;
-	return (*head);
+	}
+	return (1);
 }
 
 /**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: A pointer to the head of the linked list.
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: head of linked list
  *
- * Return: If the linked list is not a palindrome - 0.
- *         If the linked list is a palindrome - 1.
+ * Return: 1 palindrome, 0 not palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *tmp, *rev, *mid;
-	size_t size = 0, i;
-
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
-
-	tmp = *head;
-	while (tmp)
+	if (head == NULL)
 	{
-		size++;
-		tmp = tmp->next;
-	}
-
-	tmp = *head;
-	for (i = 0; i < (size / 2) - 1; i++)
-		tmp = tmp->next;
-
-	if ((size % 2) == 0 && tmp->n != tmp->next->n)
 		return (0);
-
-	tmp = tmp->next->next;
-	rev = reverse_listint(&tmp);
-	mid = rev;
-
-	tmp = *head;
-	while (rev)
-	{
-		if (tmp->n != rev->n)
-			return (0);
-		tmp = tmp->next;
-		rev = rev->next;
 	}
-	reverse_listint(&mid);
-
-	return (1);
+	return (palindrome(head, *head));
 }
