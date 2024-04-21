6c8cfb13d276d97eabba3d1d2dd8a3e763c12e47
wiza2rd <hadosh22@hotmail.com>
2024-04-21 09:41:40 -0700

    a

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,6 @@
 """fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
-
 if __name__ == "__main__":
     """fetches https://intranet.hbtn.io/status"""
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
