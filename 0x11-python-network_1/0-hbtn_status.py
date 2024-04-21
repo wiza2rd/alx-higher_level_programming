c95b2849aaf16afa8d4acfc79c6439b25d39372a
wiza2rd <hadosh22@hotmail.com>
2024-04-21 09:46:01 -0700

    a

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@
 import urllib.request
 
 if __name__ == "__main__":
-    """fetches https://intranet.hbtn.io/status"""
+    """https://intranet.hbtn.io/status"""
     with urllib.request.urlopen('http://0.0.0.0:5050/status') as response:
         html = response.read()
         html_str = html.decode('utf-8')
