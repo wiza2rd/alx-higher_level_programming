7fb15e52f8311b25dd382e7baa295f842485e630
wiza2rd <hadosh22@hotmail.com>
2024-04-21 09:48:30 -0700

    a

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ import requests
 
 if __name__ == "__main__":
     """fetches https://intranet.hbtn.io/status using requests"""
-    r = requests.get('https://intranet.hbtn.io/status')
+    r = requests.get('http://0.0.0.0:5050/status')
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
