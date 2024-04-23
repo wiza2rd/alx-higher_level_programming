9524e5c14cce47cc4f6af4a036d535fbf93d3782
wiza2rd <hadosh22@hotmail.com>
2024-04-23 09:33:29 -0700

    update

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,23 @@
 #!/usr/bin/node
-const request = require('request');
-const url = process.argv[2];
 
-request(url, function (err, data, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    let counter = 0;
-    const films = JSON.parse(body).results;
-    for (let result = 0; result < films.length; result++) {
-      const characters = films[result].characters;
-      for (let j = 0; j < characters.length; j++) {
-        if (characters[j] === 'https://swapi-api.alx-tools.com/api/films' || characters[j] === 'https://swapi-api.alx-tools.com/api/films') {
-          counter += 1;
-        }
-      }
-    }
-    console.log(counter);
+// Import the 'request' module
+const request = require('request');
+
+// Use the 'request' module to perform an HTTP GET request to the URL
+request(process.argv[2], function (error, response, body) {
+  // Check if there was no error during the HTTP request.
+  if (!error) {
+    // parse the JSON data and extract the "results" array
+    const results = JSON.parse(body).results;
+    // Use the 'reduce()' method to iterate through the movies in the 'results' array.
+    console.log(results.reduce((count, movie) => {
+      // check if there is a character with ID 18 ('/18/') in the 'characters' array.
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        // If a character with ID 18 is found, increment the count by 1.
+        ? count + 1
+        // Otherwise, keep the count unchanged.
+        : count;
+      // The 'reduce()' method starts with an initial value of 0 ('0' at the end).
+    }, 0));
   }
 });
